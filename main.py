1217a5eca66c7c8c39f47c507453de2e7e9afa8a
DanNisenson <danpablo1@gmail.com>
2023-12-03 23:01:12 +0100

    ✨run ng generate component and override html

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
+import subprocess
 from bs4 import BeautifulSoup
 
+APP_ICONS_PATH = "/Users/dan/web_development/Twinny/twinny-app/src/app/shared/ui/icons"
 
-def get_user_input(has_tried):
+
+def get_path_input(has_tried):
     while True:
         usr_input = input(
             "SVG path: " if not has_tried else "Try again or enter 'q' to quit: "
@@ -27,6 +30,20 @@ def read_html_file(file_path):
         raise
 
 
+def get_component_name():
+    usr_input = input("Enter component name in kebab-case: ")
+
+    if has_uppercase(usr_input):
+        print("Component name cannot contain uppercase letters")
+        return get_component_name()
+    else:
+        return usr_input
+
+
+def has_uppercase(input_string):
+    return any(char.isupper() for char in input_string)
+
+
 def modify_svg_elements(soup):
     svgs = soup.find_all("svg")
 
@@ -44,10 +61,14 @@ def modify_svg_elements(soup):
                 svg["stroke"] = "none"
 
 
-def save_modified_html(soup, output_path):
+def save_modified_html(soup, component_name, icons_folder):
+    full_comp_name = f"{component_name}.component.html"
+    component_path = os.path.join(icons_folder, component_name, full_comp_name)
+
     try:
-        with open(output_path, "w", encoding="utf-8") as new_file:
+        with open(component_path, "w", encoding="utf-8") as new_file:
             new_file.write(soup.prettify())
+            print(f"Override html at: {component_path}")
     except OSError as err:
         print(f"OS error: {err}")
         return False
@@ -58,20 +79,31 @@ def save_modified_html(soup, output_path):
     return True
 
 
-def transform():
+def create_component(component_name):
+    proc = subprocess.Popen(
+        ["ng", "generate", "component", component_name],
+        cwd=APP_ICONS_PATH,
+        stdout=subprocess.PIPE,
+    )
+
+    print(f"Create component at: {APP_ICONS_PATH}")
+
+    while True:
+        line = proc.stdout.readline()
+        if not line:
+            break
+
+
+def main():
     has_tried = False
 
     while True:
-        usr_input = get_user_input(has_tried)
+        file_path = get_path_input(has_tried)
 
-        if usr_input is None:
+        if file_path is None:
             return
 
-        directory, filename = os.path.split(usr_input)
-        output_filename = filename.split(".")[0] + "_ok.svg"
-        output_path = os.path.join(directory, output_filename)
-
-        html_soup = read_html_file(usr_input)
+        html_soup = read_html_file(file_path)
 
         if not html_soup:
             has_tried = True
@@ -79,15 +111,18 @@ def transform():
         else:
             break
 
+    component_name = get_component_name()
+
     if html_soup is not None:
         modify_svg_elements(html_soup)
-        success = save_modified_html(html_soup, output_path)
+        create_component(component_name)
+        success = save_modified_html(html_soup, component_name, APP_ICONS_PATH)
 
         if success:
-            print(f"Transformation successful. Modified file saved at: {output_path}")
+            print("Transformation successful. Modified file saved.")
         else:
             print("Transformation failed.")
 
 
 if __name__ == "__main__":
-    transform()
+    main()
